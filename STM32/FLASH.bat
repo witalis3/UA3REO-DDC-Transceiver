a325e81af280c7b0ee978499739d56ace538533d
XGudron <XGudron@gmail.com>
2020-07-17 11:46:07 +0300

    New flasher

--- STM32/FLASH.bat
+++ STM32/FLASH.bat
@@ -1,2 +1,2 @@
-"DFU\qmk-dfuse.exe" -d MDK-ARM\UA3REO\UA3REO.hex -r -v
+"c:\Program Files\STMicroelectronics\STM32Cube\STM32CubeProgrammer\bin\STM32_Programmer_CLI.exe" -c port=usb1 -d MDK-ARM\UA3REO\UA3REO.hex -v
 PAUSE
