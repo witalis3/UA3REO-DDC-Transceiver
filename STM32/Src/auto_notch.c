ebc97ffb254d8197559ae245153848b068e6ab5d
XGudron <XGudron@gmail.com>
2020-10-24 00:14:58 +0300

    Bug fixes

--- STM32/Src/auto_notch.c
+++ STM32/Src/auto_notch.c
@@ -24,8 +24,10 @@ void processAutoNotchReduction(float32_t *buffer, AUDIO_PROC_RX_NUM rx_id)
 	
 	arm_lms_norm_f32(&instance->lms2_Norm_instance, buffer, &instance->lms2_reference[instance->reference_index_old], instance->lms2_errsig2, buffer, AUTO_NOTCH_BLOCK_SIZE); // start LMS filter
 	
-	if (__ARM_isnanf(buffer[0]) || __ARM_isinff(buffer[0]))
+	if (__ARM_isnanf(buffer[0]) || __ARM_isinff(buffer[0]) || (buffer[0] > 2.0f) || (buffer[0] < -2.0f))
 	{
+		sendToDebug_str("auto notch err ");
+		sendToDebug_float32(buffer[0],false);
 		memset(buffer, 0x00, sizeof sizeof(float32_t) * AUTO_NOTCH_BLOCK_SIZE);
 		InitAutoNotchReduction();
 	}
