0bca495799675de3c1badb61555e5e3a9151c7d1
XGudron <XGudron@gmail.com>
2021-01-25 16:35:06 +0300

    Debug fix

--- STM32/Src/stm32h7xx_it.c
+++ STM32/Src/stm32h7xx_it.c
@@ -688,12 +688,13 @@ void TIM6_DAC_IRQHandler(void)
     TRX_STM32_TEMPERATURE = TRX_getSTM32H743Temperature();
     TRX_STM32_VREF = TRX_getSTM32H743vref();
 
+		//Save Debug variables
+		uint32_t dbg_tim6_delay = HAL_GetTick() - tim6_delay;
+    float32_t dbg_coeff = 1000.0f / (float32_t)dbg_tim6_delay;
+		dbg_FPGA_samples = (uint32_t)((float32_t)FPGA_samples * dbg_coeff);
     if (TRX.Debug_Console)
     {
-      //Save Debug variables
-      uint32_t dbg_tim6_delay = HAL_GetTick() - tim6_delay;
-      float32_t dbg_coeff = 1000.0f / (float32_t)dbg_tim6_delay;
-      dbg_FPGA_samples = (uint32_t)((float32_t)FPGA_samples * dbg_coeff);
+      //Print debug
       uint32_t dbg_WM8731_DMA_samples = (uint32_t)((float32_t)WM8731_DMA_samples / 2.0f * dbg_coeff);
       uint32_t dbg_AUDIOPROC_samples = (uint32_t)((float32_t)AUDIOPROC_samples * dbg_coeff);
       float32_t dbg_FPGA_Audio_Buffer_I_tmp = FPGA_Audio_Buffer_RX1_I[0];
