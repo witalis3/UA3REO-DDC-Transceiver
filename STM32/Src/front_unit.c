794852762508d366232e3f5107663718f67898e5
XGudron <XGudron@gmail.com>
2021-01-27 19:29:16 +0300

    Bug fixes

--- STM32/Src/front_unit.c
+++ STM32/Src/front_unit.c
@@ -538,7 +538,7 @@ void FRONTPANEL_Process(void)
 			else
 			{
 				TRX_SHIFT = 0;
-				TRX.IF_Gain = (uint8_t)(0.0f + ((1023.0f - mcp3008_value) * 60.0f / 1023.0f));
+				TRX.IF_Gain = (uint8_t)(0.0f + ((1023.0f - mcp3008_value) * 70.0f / 1023.0f));
 			}
 		}
 
