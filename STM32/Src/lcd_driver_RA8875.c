fd5b857db522b308225ce52e7d136f2366688094
XGudron <XGudron@gmail.com>
2021-05-10 13:57:36 +0300

    Add power delay

--- STM32/Src/lcd_driver_RA8875.c
+++ STM32/Src/lcd_driver_RA8875.c
@@ -57,6 +57,8 @@ inline uint16_t LCDDriver_readReg(uint16_t reg)
 //Initialise function
 void LCDDriver_Init(void)
 {
+	//Wait LCD on
+	HAL_Delay(250); //for stability
   //PLL Init
   LCDDriver_writeReg(LCD_RA8875_PLLC1, LCD_RA8875_PLLC1_PLLDIV1 + 11); //divider + multiplier
   HAL_Delay(1);
