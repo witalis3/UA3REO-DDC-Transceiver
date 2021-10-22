63542a4b35a0add7bc0d6fe8a3f8a25a7fe1e031
tisho88 <tisho88@abv.bg>
2021-10-22 10:23:23 +0200

    Added FT8 Decoder with QSO capabilities
    For now only the 3,5'' is supported. Have to rework the GUI functions for the 7" as well

---
+++ STM32/Src/FT8/gen_ft8.h
@@ -0,0 +1,16 @@
+/*
+ * gen_ft8.h
+ *
+ *  Created on: Oct 20, 2021
+ *  Author: Tihomir (DB5AT)
+ */
+
+#ifndef GEN_FT8_H_
+#define GEN_FT8_H_
+
+#include "arm_math.h"
+
+
+void  set_message(uint16_t index);
+
+#endif /* GEN_FT8_H_ */
