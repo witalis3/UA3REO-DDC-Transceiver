c6b6b7d3d3d0fa162415b2e5d800a2ee2fe5c6e9
XGudron <XGudron@gmail.com>
2022-03-03 22:47:40 +0300

    mem update

--- STM32/Src/functions.c
+++ STM32/Src/functions.c
@@ -626,16 +626,29 @@ void dma_memset32(void *dest, uint32_t val, uint32_t size)
 	Aligned_CleanDCache_by_Addr(&dma_memset32_reg, sizeof(dma_memset32_reg));
 	Aligned_CleanDCache_by_Addr(dest, size * 4);
 
-	HAL_MDMA_Start(&hmdma_mdma_channel44_sw_0, (uint32_t)&dma_memset32_reg, (uint32_t)dest, 4 * size, 1);
-	SLEEPING_MDMA_PollForTransfer(&hmdma_mdma_channel44_sw_0);
+	uint32_t max_block = DMA_MAX_BLOCK / 4;
+	uint32_t *current_dest = (uint32_t *)dest;
+	uint32_t estimated = size;
+	while (estimated > 0)
+	{
+		uint32_t block_size = (estimated > max_block) ? max_block : estimated;
+		HAL_MDMA_Start(&hmdma_mdma_channel44_sw_0, (uint32_t)&dma_memset32_reg, (uint32_t)current_dest, 4 * block_size, 1);
+		SLEEPING_MDMA_PollForTransfer(&hmdma_mdma_channel44_sw_0);
+		estimated -= block_size;
+		current_dest += block_size;
+	}
 
 	Aligned_CleanInvalidateDCache_by_Addr(dest, size * 4);
 	dma_memset32_busy = false;
 
 	/*uint32_t *pDst = (uint32_t *)dest;
+	uint8_t errors = 0;
 	for(uint32_t i = 0; i < size; i++)
-		if(pDst[i] != val)
-			println(i);*/
+		if(pDst[i] != val && errors < 3)
+		{
+			println(i);
+			errors++;
+		}*/
 }
 
 void memset16(void *dest, uint16_t val, uint32_t size)
@@ -669,13 +682,6 @@ void dma_memset(void *dest, uint8_t val, uint32_t size)
 		uint32_t val32 = (val << 24) | (val << 16) | (val << 8) | (val << 0);
 		uint32_t block32 = size / 4;
 		uint32_t block8 = size % 4;
-		uint32_t max_block = DMA_MAX_BLOCK / 4;
-		while (block32 > max_block)
-		{
-			dma_memset32(pDst, val32, max_block);
-			block32 -= max_block;
-			pDst += max_block * 4;
-		}
 		dma_memset32(pDst, val32, block32);
 
 		//right align
