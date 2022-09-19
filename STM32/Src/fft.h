0c590c8b01650698fb409a9868d7e36a9d3daacf
dmitry.panin <d.panin@skyeng.ru>
2022-09-19 23:11:12 +0300

    FFT Compressor update

--- STM32/Src/fft.h
+++ STM32/Src/fft.h
@@ -23,7 +23,7 @@
 
 #define FFT_HALF_SIZE (FFT_SIZE / 2)
 #define FFT_DOUBLE_SIZE_BUFFER (FFT_SIZE * 2)                                                                                                                // Buffer size for FFT calculation                                                                                                     // average threshold of the FFT signal
-#define FFT_COMPRESS_INTERVAL 0.9f                                                                                                                           // compress interval of the FFT signal
+#define FFT_COMPRESS_INTERVAL 0.95f                                                                                                                           // compress interval of the FFT signal
 #define FFT_STEP_COEFF 10.0f                                                                                                                                 // step coefficient for auto-calibration of the FFT signal (more - slower)
 #define FFT_HZ_IN_PIXEL (float32_t)((float32_t)TRX_GetRXSampleRate * ((float32_t)FFT_USEFUL_SIZE / (float32_t)FFT_SIZE) / (float32_t)LAYOUT->FFT_PRINT_SIZE) // hertz per FFT pixel RX
 #define FFT_TX_HZ_IN_PIXEL (float32_t)((float32_t)TRX_SAMPLERATE * ((float32_t)FFT_USEFUL_SIZE / (float32_t)FFT_SIZE) / (float32_t)LAYOUT->FFT_PRINT_SIZE)   // hertz per FFT pixel TX
