2b50055cad55aa4407867ffd53dbbeb11598e150
dmitry.panin <d.panin@skyeng.ru>
2022-10-11 22:42:01 +0300

    Update defaults

--- STM32/Src/settings.c
+++ STM32/Src/settings.c
@@ -171,7 +171,7 @@ void LoadSettings(bool clear)
 		TRX.FRQ_FAST_STEP = 100;			// frequency tuning step by the main encoder in FAST mode
 		TRX.FRQ_ENC_STEP = 25000;			// frequency tuning step by main add. encoder
 		TRX.FRQ_ENC_FAST_STEP = 50000;		// frequency tuning step by main add. encoder in FAST mode
-		TRX.FRQ_ENC_WFM_STEP_KHZ = 100;		// frequency WFM tuning step by the main encoder
+		TRX.FRQ_ENC_WFM_STEP_KHZ = 20;		// frequency WFM tuning step by the main encoder
 		TRX.FRQ_CW_STEP_DIVIDER = 4;		// Step divider for CW mode
 		TRX.Debug_Type = TRX_DEBUG_OFF;		// Debug output to DEBUG / UART port
 		TRX.BandMapEnabled = true;			// automatic change of mode according to the range map
