ee5f000d6b3b1a093f7cf5ef082bd0c996e94910
dmitry.panin <d.panin@skyeng.ru>
2023-03-13 21:04:27 +0300

    ClkSource update

--- STM32/WOLF-2/Src/main.c
+++ STM32/WOLF-2/Src/main.c
@@ -462,17 +462,8 @@ void PeriphCommonClock_Config(void) {
 
 	/** Initializes the peripherals clock
 	 */
-	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_SPI3 | RCC_PERIPHCLK_SPI2 | RCC_PERIPHCLK_CKPER;
-	PeriphClkInitStruct.PLL2.PLL2M = 1;
-	PeriphClkInitStruct.PLL2.PLL2N = 48;
-	PeriphClkInitStruct.PLL2.PLL2P = 5;
-	PeriphClkInitStruct.PLL2.PLL2Q = 2;
-	PeriphClkInitStruct.PLL2.PLL2R = 2;
-	PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_3;
-	PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;
-	PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
+	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CKPER;
 	PeriphClkInitStruct.CkperClockSelection = RCC_CLKPSOURCE_HSI;
-	PeriphClkInitStruct.Spi123ClockSelection = RCC_SPI123CLKSOURCE_PLL2;
 	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
 		Error_Handler();
 	}
