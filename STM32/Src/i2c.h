6aa75b4609a72dcf3f472b29fdfa6eda17178e9e
dmitry.panin <d.panin@skyeng.ru>
2023-03-25 21:36:34 +0300

    I2C fix

--- STM32/Src/i2c.h
+++ STM32/Src/i2c.h
@@ -20,10 +20,15 @@
 #define I2C_DELAY                                   \
 	for (uint32_t wait_i = 0; wait_i < 400; wait_i++) \
 		__asm("nop");
-#define I2C_OUTPUT_MODE GPIO_MODE_OUTPUT_PP
 #define I2C_INPUT_MODE GPIO_MODE_INPUT
 #define I2C_PULLS GPIO_PULLUP
 #define I2C_GPIO_SPEED GPIO_SPEED_FREQ_LOW
+
+#ifdef FRONTPANEL_WOLF_2
+	#define I2C_OUTPUT_MODE GPIO_MODE_OUTPUT_PP
+#else
+	#define I2C_OUTPUT_MODE GPIO_MODE_OUTPUT_OD
+#endif
 
 typedef struct {
 	GPIO_TypeDef *SDA_PORT;
