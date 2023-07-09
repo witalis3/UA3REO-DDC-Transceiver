81168c73144fc8c5ee4e172716b41b5515e41b29
XGudron <XGudron@gmail.com>
2023-07-09 16:24:28 +0300

    W-Mini bottom menu redraw fix

--- STM32/WOLF-Mini/Src/lcd.c
+++ STM32/WOLF-Mini/Src/lcd.c
@@ -164,6 +164,10 @@ static void LCD_displayTopButtons(bool redraw) { // display the top buttons
 	if (redraw) {
 		LCD_UpdateQuery.TopButtonsRedraw = false;
 	}
+	
+	// redraw bottom after top
+	LCD_UpdateQuery.BottomButtons = true;
+	
 	LCD_busy = false;
 }
 
