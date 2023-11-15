0a17dbe7e7dd64a6a3dc13e7b03a95f03d4af00f
XGudron <XGudron@gmail.com>
2023-11-15 23:23:03 +0300

    Version update

--- STM32/Src/settings.h
+++ STM32/Src/settings.h
@@ -8,7 +8,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define STM32_VERSION_STR "8.6.3" // current STM32 version
+#define STM32_VERSION_STR "8.6.4-dev" // current STM32 version
 
 #if defined(FRONTPANEL_MINI)
 #define FPGA_VERSION_STR "6.8.0" // needed FPGA version Wolf-Mini
